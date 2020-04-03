6acefb9d4d941c46646ce99f7431abfa78e115fb
Henry Lee <henryleeworld@gmail.com>
2020-04-03 18:30:23 +0800

    feat:升級至 laravel 7

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('is_active');
             $table->rememberToken();
+            $table->boolean('is_active');
             $table->timestamps();
         });
     }
