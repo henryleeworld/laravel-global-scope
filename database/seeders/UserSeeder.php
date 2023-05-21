0ec3ca1541128d6721490747851918ee4a0a66ae
Henry Lee <henryleeworld@gmail.com>
2023-05-22 00:03:56 +0800

    feat:升級至 laravel 10

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,10 +5,12 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
-class UsersTableSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     public function run()
     {
-        User::factory()->times(1000)->create();
+        User::factory()
+            ->count(1000)
+            ->create();
     }
 }
